64a47bec777cafab63ad008d607821f347efd45a
hugo <hugo.perier@cern.ch>
2024-08-03 15:50:45 +0200

    wip connection

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@ if [ -z "$(docker network ls -qf name=^entropic$)" ]; then
   docker network create entropic >/dev/null
 fi
 
-COMPOSE_HTTP_TIMEOUT=120 docker-compose -f docker-compose.dev.yml up -d --force-recreate
+COMPOSE_HTTP_TIMEOUT=120 docker compose -f docker-compose.dev.yml up -d --force-recreate
